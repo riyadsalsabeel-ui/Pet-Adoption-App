ec79dede82469a1473394e4b3b8a78fe63987c58
salsabeel77 <riyadsalsabeel@gmail.com>
2025-10-14 11:07:33 +0300

    update

--- pet_adoption/urls.py
+++ pet_adoption/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("core.urls", "core"), namespace="core")),
-    path("accounts/", include("django.contrib.auth.urls")),  # login/logout/password views
+    path("accounts/", include("django.contrib.auth.urls")),  
 ]
 
 if settings.DEBUG:
